14a34eea84f482fc83850f07972dd2bc73598fae
Orion Henry <orion.henry@gmail.com>
2021-01-01 21:11:54 -0500

    adding debugging info

--- backend/wasm/index.d.ts
+++ backend/wasm/index.d.ts
@@ -1,59 +1,61 @@
 /* tslint:disable */
 /* eslint-disable */
+/**
+*/
 export class State {
   free(): void;
 /**
-* @param {Array<any>} changes 
-* @returns {Array<any>} 
+* @param {Array<any>} changes
+* @returns {Array<any>}
 */
   applyChanges(changes: Array<any>): Array<any>;
 /**
-* @param {Array<any>} changes 
-* @returns {any} 
+* @param {Array<any>} changes
+* @returns {any}
 */
   loadChanges(changes: Array<any>): any;
 /**
-* @param {any} change 
-* @returns {Array<any>} 
+* @param {any} change
+* @returns {Array<any>}
 */
   applyLocalChange(change: any): Array<any>;
 /**
-* @returns {any} 
+* @returns {any}
 */
   getPatch(): any;
 /**
-* @returns {any} 
+* @returns {any}
 */
   getHeads(): any;
 /**
-* @param {any} have_deps 
-* @returns {Array<any>} 
+* @param {any} have_deps
+* @returns {Array<any>}
 */
   getChanges(have_deps: any): Array<any>;
 /**
-* @param {any} actorid 
-* @returns {Array<any>} 
+* @param {any} actorid
+* @returns {Array<any>}
 */
   getChangesForActor(actorid: any): Array<any>;
 /**
-* @returns {any} 
+* @returns {any}
 */
   getMissingDeps(): any;
 /**
-* @returns {State} 
+* @returns {State}
 */
   clone(): State;
 /**
-* @returns {any} 
+* @returns {any}
 */
   save(): any;
 /**
-* @param {any} data 
-* @returns {State} 
+* @param {any} data
+* @returns {State}
 */
   static load(data: any): State;
 /**
-* @returns {State} 
+* @returns {State}
 */
   static new(): State;
 }
